1da76d5320d35bd3a32f22f3c57f97d6a3d1e458
Azima Ashyrkulova <azimo4ka@gmail.com>
2021-05-19 15:46:30 +0200

    Create task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const validationInputRef = document.querySelector('#validation-input');
+
+validationInputRef.addEventListener('blur', onValidate);
+function onValidate(e) {
+    if (+e.target.dataset.length === e.target.value.length) {
+        changeClass('valid', 'invalid')
+    } else { changeClass('invalid','valid') }
+};
+
+function changeClass(add, rem) {
+    validationInputRef.classList.remove(rem);
+    validationInputRef.classList.add(add);
+}
