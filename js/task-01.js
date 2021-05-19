027ccb589d21528ab90225a04964b4ed5bd41886
Azima Ashyrkulova <azimo4ka@gmail.com>
2021-05-19 15:46:16 +0200

    Create task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const ulRef = document.querySelector('#categories');
+console.log(`V spiske ${ulRef.children.length} kategorii`);
+[...ulRef.children].forEach((el) => {
+    console.log('Kategoria:', el.children[0].textContent);
+    console.log('Kolichestvo elementov:', el.children[1].children.length);
+});
